842863e07863a92b2e2a813150634a41bce19052
geokal <kalpgiorgos@gmail.com>
2023-02-20 21:06:59 +0200

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,23 @@
-import React from "react"
-import { GetStaticProps } from "next"
-import Layout from "../components/Layout"
-import Post, { PostProps } from "../components/Post"
+import React from 'react'
+import { GetStaticProps } from 'next'
+import Layout from '../components/Layout'
+import Post, { PostProps } from '../components/Post'
 
+import prisma from '../lib/prisma'
+
+// index.tsx
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+  const feed = await prisma.post.findMany({
+    where: { published: true },
+    include: {
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
-      },
-    },
-  ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
+        select: { name: true }
+      }
+    }
+  })
+  return {
+    props: { feed },
+    revalidate: 10
   }
 }
 
@@ -26,14 +25,14 @@ type Props = {
   feed: PostProps[]
 }
 
-const Blog: React.FC<Props> = (props) => {
+const Blog: React.FC<Props> = props => {
   return (
     <Layout>
-      <div className="page">
+      <div className='page'>
         <h1>Public Feed</h1>
         <main>
-          {props.feed.map((post) => (
-            <div key={post.id} className="post">
+          {props.feed.map(post => (
+            <div key={post.id} className='post'>
               <Post post={post} />
             </div>
           ))}
